521baf97d2b5b96cbe2a4022b4b9cbe4cd3470fb
Matthew Alexander P <39817115+ProProcer@users.noreply.github.com>
2026-08-14 10:37:31 +0700

    add the shell command to export the dataset

--- sql/03_build_order_delivery_dataset.sql
+++ sql/03_build_order_delivery_dataset.sql
@@ -1,3 +1,4 @@
+-- python scripts/export_dataset.py -i sql/03_build_order_delivery_dataset.sql -o data/processed/order_delivery_datset.csv
 SELECT 
 	o.order_id AS order_id,
 	ANY_VALUE(cg.geolocation_lat) AS customer_lat,
