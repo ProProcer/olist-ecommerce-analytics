6c2be261ab0955b4894dd9d892c5b45b4f12aec4
Matthew Alexander P <39817115+ProProcer@users.noreply.github.com>
2026-08-24 15:47:36 +0700

    add class of the zscore model

---
+++ src/models/zscore.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+import scipy.stats as ss
+
+class ZScore:
+    def __init__(self, alpha = 0.05):
+        self.alpha = alpha
+
+    def fit(self, X, y):
+        X = np.array(X)
+        y = pd.Series(y)
+        
+        self.mean = y.groupby(list(X.T)).mean()
+        self.std = y.groupby(list(X.T)).std()
+        
+    def predict(self, X, y):
+        X = np.array(X)
+        y = np.array(y)
+        if X.shape[1] > 1:
+            center = self.mean[list(map(tuple, X))]
+        else: 
+            center = self.mean[X[:, 0]]
+        upper_bound = center + ss.norm.ppf(1 - self.alpha) * self.std
+
+        return {
+            'is_outlier' : (upper_bound < y).astype(int),
+            'center' : center
+        }
