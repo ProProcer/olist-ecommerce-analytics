9f5fc0352bd134217342a0c25369423e58aeabc0
Matthew Alexander P <39817115+ProProcer@users.noreply.github.com>
2026-08-25 10:48:16 +0700

    make load data more flexible by having list of transformation as an argument

--- src/data/loader.py
+++ src/data/loader.py
@@ -1,9 +1,11 @@
 import pandas as pd
+from hydra.utils import instantiate, UNSAFE_ALLOW_ALL_TARGETS
 
-def load_data(path, timestamp, numerical_cols, categorical_cols):
+def load_data(path, transforms):
     df = pd.read_csv(path)
-    df = df[[timestamp] + numerical_cols + categorical_cols]
-    df[timestamp] = pd.to_datetime(df[timestamp])
-    df[numerical_cols] = df[numerical_cols].astype('float32')
-    df[categorical_cols] = df[categorical_cols].astype(str).astype('category')
+
+    for fn in transforms:
+        fn = instantiate(fn, _target_whitelist_ = UNSAFE_ALLOW_ALL_TARGETS)
+        df = df.pipe(fn)
+
     return df
