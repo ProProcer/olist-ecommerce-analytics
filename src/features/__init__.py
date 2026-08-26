ce6762e85757f448d6306bd2aaba598281dea8b6
Matthew Alexander P <39817115+ProProcer@users.noreply.github.com>
2026-08-26 20:24:15 +0700

    move the feature transformer factory to a dedicated python file

--- src/features/__init__.py
+++ src/features/__init__.py
@@ -1,26 +1,3 @@
-from sklearn.compose import ColumnTransformer 
-from sklearn.pipeline import Pipeline
+from src.features.pipeline import get_feature_transformer
 
-def get_feature_transformer(**kwargs):
-    group_transformer_list = []
-    for group_name, group in kwargs.items():
-        transformers = [(name, trans, slice(None)) for name, trans in group.items() if name != 'passthrough']
-        
-        group_transformer = ColumnTransformer(
-            transformers,
-            verbose_feature_names_out= True
-        )
-        if 'passthrough' in group:
-            passthrough_cols = list(group.get('passthrough'))
-            group_transformer = ColumnTransformer(
-                [
-                    (group_name, group_transformer, slice(None)), 
-                    ('passthrough', 'passthrough', passthrough_cols)
-                ],
-                verbose_feature_names_out= False
-            )
-        
-        
-        group_transformer_list.append((group_name, group_transformer))
-
-    return Pipeline(group_transformer_list).set_output(transform = 'pandas')
+__all__ = ["get_feature_transformer"]
