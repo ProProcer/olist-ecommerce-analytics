c93a9856cab5ca43f78520c5f360e28d45543d01
Matthew Alexander P <39817115+ProProcer@users.noreply.github.com>
2026-08-29 13:46:22 +0700

    refactor code by creating a parent class of GroupEstimator

--- src/models/group.py
+++ src/models/group.py
@@ -3,35 +3,9 @@ import pandas as pd
 import scipy.stats as ss
 from src.schemas.predictions import AnomalyPredictions
 from sklearn.base import BaseEstimator
+from typing import Tuple
 
-class ZScore(BaseEstimator):
-    def __init__(self, alpha):
-        self.alpha = alpha
-    
-
-    def fit(self, X, y):
-        X = np.array(X)
-        y = pd.Series(y)
-        
-        self.mean = y.groupby(list(X.T)).mean()
-        self.std = y.groupby(list(X.T)).std()
-        
-    def predict(self, X) -> AnomalyPredictions:
-        X = np.array(X)
-        if X.shape[1] > 1:
-            center = self.mean[list(map(tuple, X))]
-        else: 
-            center = self.mean[X[:, 0]]
-        upper_bound = center + ss.norm.ppf(1 - self.alpha) * self.std
-
-        preds = AnomalyPredictions(
-            center = center.to_numpy(),
-            upper_bound = upper_bound.to_numpy()
-        )
-
-        return preds
-
-class TScore(BaseEstimator):
+class GroupEstimator(BaseEstimator):
     def __init__(self, alpha):
         self.alpha = alpha
 
@@ -39,20 +13,22 @@ class TScore(BaseEstimator):
         X = np.asarray(X)
         y = pd.Series(y)
 
-        keys = []
+        indices = []
         upper_bounds = []
         centers = []
-        for key, ser in y.groupby(list(X.T)):
-            keys.append(key)
-            t_params = ss.t.fit(ser)
-            upper_bounds.append(ss.t.ppf(1 - self.alpha, *t_params))
-            centers.append(np.mean(ser))
+        for i, group in y.groupby(list(X.T)):
+            indices.append(i)
+            upper_bounds.append(self._get_bounds(group))
+            centers.append(np.mean(group))
 
         self.preds = pd.DataFrame(
             {'upper_bound' : upper_bounds, 'center' : centers},
-            index = keys
+            index = indices
         )
-        
+
+    def _get_upper_bound(self, a : np.ndarray) -> Tuple[float]:
+        raise NotImplementedError()
+
     def predict(self, X):
         X = np.array(X)
         
@@ -63,38 +39,19 @@ class TScore(BaseEstimator):
             upper_bound = preds.upper_bound.to_numpy()
         )
 
-class EmpiricalQuantile(BaseEstimator):
-    def __init__(self, alpha):
-        self.alpha = alpha
+class ZScore(GroupEstimator):
+    def _get_bounds(self, a):
+        params = ss.norm.fit(a)
+        return ss.norm.ppf(1 - self.alpha, *params)
 
-    def fit(self, X, y):
-        X = np.asarray(X)
-        y = pd.Series(y)
+class TScore(GroupEstimator):
+    def _get_bounds(self, a):
+        params = ss.t.fit(a)
+        return ss.t.ppf(1 - self.alpha, *params)
 
-        keys = []
-        upper_bounds = []
-        centers = []
-
-        for key, ser in y.groupby(list(X.T)):
-            keys.append(key)
-            h = 1 + (len(ser) - 1) * 0.95
-            g = h - int(h)
-            sorted_ser = np.sort(ser)
-
-            centers.append(np.mean(ser))
-            upper_bounds.append(sorted_ser[int(h)] + (sorted_ser[int(h) + 1] - sorted_ser[int(h)]) * g)
-
-        self.preds = pd.DataFrame(
-            {'upper_bound' : upper_bounds, 'center' : centers},
-            index = keys
-        )
-
-    def predict(self, X):
-        X = np.array(X)
-        
-        preds = self.preds.loc[list(map(tuple, X))]
-
-        return AnomalyPredictions(
-            center = preds.center.to_numpy(),
-            upper_bound = preds.upper_bound.to_numpy()
-        )
+class EmpiricalQuantile(GroupEstimator):
+    def _get_bounds(self, a):
+        h = 1 + (len(a) - 1) * 0.95
+        g = h - int(h)
+        sorted_a = np.sort(a)
+        return sorted_a[int(h)] + (sorted_a[int(h) + 1] - sorted_a[int(h)]) * g
