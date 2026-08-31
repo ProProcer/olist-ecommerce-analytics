27238307fb31d82c6d1d659bc4f8d3b9c3a4e818
Matthew Alexander P <39817115+ProProcer@users.noreply.github.com>
2026-08-31 14:47:32 +0700

    add script to evaluate model on test data

---
+++ evaluate.py
@@ -0,0 +1,25 @@
+import hydra
+from omegaconf import DictConfig
+from src.data.loader import load_test_data
+from hydra.utils import instantiate, UNSAFE_ALLOW_ALL_TARGETS
+from src.utils.resolvers import register_custom_resolvers
+
+@hydra.main(config_path="configs", config_name="config")
+def main(cfg: DictConfig):
+    register_custom_resolvers()
+
+    df = load_test_data(cfg.data, cfg.split)
+
+    validator = instantiate(cfg.validator, _target_whitelist_ = UNSAFE_ALLOW_ALL_TARGETS)
+
+    result = validator.run(df) 
+
+    tracker = instantiate(cfg.tracker, _target_whitelist_ = UNSAFE_ALLOW_ALL_TARGETS)
+    tracker.log_run(
+        run_name = cfg.run_name + "_test", 
+        config = cfg, 
+        result = result
+    )
+
+if __name__ == "__main__":
+    main()
