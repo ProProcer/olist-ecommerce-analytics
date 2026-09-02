1fa2359b11ee515789cef74b9adf277580d64a4e
Matthew Alexander P <39817115+ProProcer@users.noreply.github.com>
2026-09-02 11:00:05 +0700

    add interaction term on log linear regression

--- src/features/engineering.py
+++ src/features/engineering.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from sklearn.preprocessing import FunctionTransformer
 
 def dow(cols):
@@ -13,8 +14,33 @@ def is_equal(cols):
             x[cols]
             .eq(x[cols].iloc[:, 0], axis=0)
             .all(axis=1)
-            .to_frame(name="is_within_state")
+            .to_frame(name="")
         )
 
     return FunctionTransformer(transform)
-    
+
+def interaction_categorical(cols):
+    cols = list(cols)
+
+    def transform(x):
+        x = x.astype(str)
+        return x.sum(axis = 1).to_frame(name="")
+
+    return FunctionTransformer(transform)
+
+def dow_is_within_state_design(cols):
+    cols = list(cols)
+    ts_col, seller_col, customer_col = cols
+
+    def transform(x):
+        dow = x[ts_col].dt.dayofweek.astype(int)
+        within = x[seller_col].eq(x[customer_col])
+
+        out = {f'dow_{d}' : (dow == d).astype(int) for d in range(1, 7)}
+        out['is_within_state'] = within.astype(int)
+        for d in range(1, 7):
+            out[f'dow_{d}_x_within'] = ((dow == d) & within).astype(int)
+
+        return pd.DataFrame(out)
+
+    return FunctionTransformer(transform)
